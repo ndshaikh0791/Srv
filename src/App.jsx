02a8685a699e6b4318ac919971999bce3b1df12c
naushad856 <naushad.shaikh@sargsoftech.com>
2025-12-25 18:32:28 +0530

    Add full project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SRVHeader from './components/SRVHeader';
+
+function App() {
+  return (
+    <div className="App">
+      <SRVHeader />
+    </div>
+  );
+}
+
+export default App;
